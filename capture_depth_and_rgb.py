bf02bf0c8502f1444086bc9f9383940dc1d5fa54
kevinyoussef <kryousse@ucsd.edu>
2020-02-06 10:49:17 -0800

    11 adn 8 bit csv files

--- capture_depth_and_rgb.py
+++ capture_depth_and_rgb.py
@@ -45,19 +45,26 @@ def main():
     cv2.imwrite(title + '_RBG.png', rgbIMG)
     cv2.imwrite(title + '_Depth.png', depthIMG)
     print(f'\n \n \n \n \n  {depthIMG.shape}  \n \n \n \n \n')
-    with open(title + '_Depth.csv', 'w+') as csv_file:
+    with open(title + '_Depth_11.csv', 'w+') as csv_file:
         file_writer = csv.writer(csv_file, delimiter = ',')
         for i in range(len(depthCSV)):
             file_writer.writerow(depthCSV[i])
     print(f'\n \n \n \n \n  DONE 1 \n \n \n \n \n')
     
+
+    with open(title + '_Depth_8.csv', 'w+') as csv_file:
+        file_writer = csv.writer(csv_file, delimiter = ',')
+        for i in range(len(depthIMG)):
+            file_writer.writerow(depthIMG[i])
+
+    '''    
     with open(title + '_RGB.csv', 'w+') as csv_file:
         file_writer = csv.writer(csv_file, delimiter = ',')
         for i in range(len(rgbCSV)):
             file_writer.writerow(rgbCSV[i])
             
     print(f'\n \n \n \n \n  DONE 2 \n \n \n \n \n')
-
+    '''
     os.chdir(path)
     print("depth")
     print(depth)
